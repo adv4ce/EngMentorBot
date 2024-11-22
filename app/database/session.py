9a51ba3f70240df9044559aaa8a70120bc54a88e
adv4ce <lockindock@yandex.ru>
2024-11-23 02:53:06 +0300

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-
 from . import params as p
 
-engine = create_async_engine(url=f'postgresql+asyncpg://{p.params['login']}:{p.params['password']}@localhost:{p.params['localhost']}/{p.params['namedb']}')
+db_url = f'postgresql+asyncpg://{p.params["login"]}:{p.params["password"]}@localhost:{p.params["localhost"]}/{p.params["namedb"]}'
+
+engine = create_async_engine(url=db_url)
 
 async_session = async_sessionmaker(engine)
