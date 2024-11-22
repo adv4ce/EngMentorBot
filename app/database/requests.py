61f9ac20ae0edf3f1f721fb18de39e428d3d5e72
adv4ce <lockindock@yandex.ru>
2024-11-23 02:52:03 +0300

    Update requests.py

--- app/database/requests.py
+++ app/database/requests.py
@@ -6,6 +6,7 @@ from sqlalchemy.future import select
 from sqlalchemy.sql import func
 from random import randint
 
+
 async def set_user(tg_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
@@ -30,7 +31,7 @@ async def get_words(tg_id):
 
             return words
         except Exception as e:
-            print(f'Ошибка {e} в функции get_words. Входные данные: tg_id={tg_id}')
+            print(f"Ошибка {e} в функции get_words. Входные данные: tg_id={tg_id}")
 
 
 async def get_word_info(tg_id, words):
@@ -38,12 +39,15 @@ async def get_word_info(tg_id, words):
         user_id = await session.scalar(select(User.id).where(User.tg_id == tg_id))
         try:
             word = await session.execute(
-                select(Word.eng, Word.rus).where(or_(Word.eng == words, Word.rus == words), Word.user_id == user_id)
+                select(Word.eng, Word.rus).where(
+                    or_(Word.eng == words, Word.rus == words), Word.user_id == user_id
+                )
             )
             return list(word)[0]
         except Exception as e:
-            print(f'Ошибка {e} в функции get_word_info. Входные данные: tg_id={tg_id}, words={words}')
-        
+            print(
+                f"Ошибка {e} в функции get_word_info. Входные данные: tg_id={tg_id}, words={words}"
+            )
 
 
 async def set_words(tg_id, words):
@@ -61,7 +65,10 @@ async def set_words(tg_id, words):
                 session.add(Word(eng=words[0], rus=words[1], user_id=user_id))
                 await session.commit()
         except Exception as e:
-            print(f'Ошибка {e} в функции set_words. Входные данные: tg_id={tg_id}, words={words}')
+            print(
+                f"Ошибка {e} в функции set_words. Входные данные: tg_id={tg_id}, words={words}"
+            )
+
 
 async def delete_words(tg_id, word_del):
     async with async_session() as session:
@@ -75,31 +82,44 @@ async def delete_words(tg_id, word_del):
             )
             await session.commit()
         except Exception as e:
-            print(f'Ошибка {e} в функции delete_words. Входные данные: tg_id={tg_id}, word_del={word_del}')
+            print(
+                f"Ошибка {e} в функции delete_words. Входные данные: tg_id={tg_id}, word_del={word_del}"
+            )
+
 
 async def check_rus_answer(tg_id, word_question, user_answer):
     async with async_session() as session:
         try:
             user_id = await session.scalar(select(User.id).where(User.tg_id == tg_id))
             check_word = await session.scalar(
-                select(Word.eng).where(Word.rus.ilike(word_question), Word.user_id == user_id)
+                select(Word.eng).where(
+                    Word.rus.ilike(word_question), Word.user_id == user_id
+                )
             )
             right_answer = check_word
             return True if right_answer == user_answer else False
         except Exception as e:
-            print(f'Ошибка {e} в функции check_rus_answer. Входные данные: tg_id={tg_id}, word_question={word_question}, user_answer={user_answer}')
+            print(
+                f"Ошибка {e} в функции check_rus_answer. Входные данные: tg_id={tg_id}, word_question={word_question}, user_answer={user_answer}"
+            )
+
 
 async def check_eng_answer(tg_id, word_question, user_answer):
     async with async_session() as session:
         try:
             user_id = await session.scalar(select(User.id).where(User.tg_id == tg_id))
             check_word = await session.scalar(
-                select(Word.rus).where(Word.eng.ilike(word_question), Word.user_id == user_id)
+                select(Word.rus).where(
+                    Word.eng.ilike(word_question), Word.user_id == user_id
+                )
             )
             right_answer = check_word
             return True if right_answer == user_answer else False
         except Exception as e:
-            print(f'Ошибка {e} в функции check_eng_answer. Входные данные: tg_id={tg_id}, word_question={word_question}, user_answer={user_answer}')
+            print(
+                f"Ошибка {e} в функции check_eng_answer. Входные данные: tg_id={tg_id}, word_question={word_question}, user_answer={user_answer}"
+            )
+
 
 async def random_words(tg_id):
     async with async_session() as session:
@@ -111,7 +131,9 @@ async def random_words(tg_id):
 
             return list(words)[randint(0, len(list(await get_words(tg_id))) - 1)]
         except Exception as e:
-            print(f'Ошибка {e} в функции random_words. Входные данные: tg_id={tg_id}')
+            print(f"Ошибка {e} в функции random_words. Входные данные: tg_id={tg_id}")
+
+
 async def add_past_word(tg_id, add_word):
     async with async_session() as session:
         try:
@@ -135,22 +157,34 @@ async def add_past_word(tg_id, add_word):
 
             return True
         except Exception as e:
-            print(f'Ошибка {e} в функции add_past_word. Входные данные: tg_id={tg_id}, words={add_word}')
+            print(
+                f"Ошибка {e} в функции add_past_word. Входные данные: tg_id={tg_id}, words={add_word}"
+            )
+
 
 async def change_user_mode(tg_id, state):
     async with async_session() as session:
         try:
             user_id = await session.scalar(select(User.id).where(User.tg_id == tg_id))
-            await session.execute(update(Mode).where(Mode.user_id == user_id).values(state=state))
+            await session.execute(
+                update(Mode).where(Mode.user_id == user_id).values(state=state)
+            )
             await session.commit()
         except Exception as e:
-            print(f'Ошибка {e} в функции change_user_mode. Входные данные: tg_id={tg_id}')
+            print(
+                f"Ошибка {e} в функции change_user_mode. Входные данные: tg_id={tg_id}"
+            )
+
 
 async def check_user_mode(tg_id):
     async with async_session() as session:
         try:
             user_id = await session.scalar(select(User.id).where(User.tg_id == tg_id))
-            cur_state = await session.scalar(select(Mode.state).where(Mode.user_id == user_id))
+            cur_state = await session.scalar(
+                select(Mode.state).where(Mode.user_id == user_id)
+            )
             return cur_state
         except Exception as e:
-            print(f'Ошибка {e} в функции check_user_mode. Входные данные: tg_id={tg_id}')
+            print(
+                f"Ошибка {e} в функции check_user_mode. Входные данные: tg_id={tg_id}"
+            )
